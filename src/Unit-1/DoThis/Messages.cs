a57c0bbe8803e4636672f69c7e7407ee2ff9660e
Alexander Martinkyan <undisclose@gmail.com>
2016-04-06 23:24:17 +0300

    Unit 1, Lessons 1,2 are completed

---
+++ src/Unit-1/DoThis/Messages.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinTail
+{
+    public class Messages
+    {
+        #region Neutral/system messages
+
+        /// <summary>
+        /// Marker class for continue processing.
+        /// </summary>
+        public class ContinueProcessing
+        {
+        }
+
+        #endregion
+
+        #region Success messages
+
+        /// <summary>
+        /// Base class for signalling that user's input was valid.
+        /// </summary>
+        public class InputSuccess
+        {
+            public string Reason { get; }
+
+            public InputSuccess(string reason)
+            {
+                Reason = reason;
+            }
+        }
+        #endregion
+
+        #region Error messages
+
+        /// <summary>
+        /// Base class for signaling that user's input was invalid.
+        /// </summary>
+        public class InputError
+        {
+            public string Reason { get; }
+
+            public InputError(string reason)
+            {
+                Reason = reason;
+            }
+        }
+
+        /// <summary>
+        /// User provided blank input.
+        /// </summary>
+        public class NullInputError : InputError
+        {
+            public NullInputError(string reason)
+                : base(reason)
+            {
+            }
+        }
+
+        public class ValidationError : InputError
+        {
+            public ValidationError(string reason) 
+                : base(reason)
+            {
+            }
+        }
+
+        #endregion
+    }
+}
